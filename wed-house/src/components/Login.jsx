71fdea4f6fef30b6a59a9f99c5dc41b61d263976
anjadkt <anjad076@gmail.com>
2025-09-17 09:20:09 +0530

    form validation set up

--- wed-house/src/components/Login.jsx
+++ wed-house/src/components/Login.jsx
@@ -1,9 +1,10 @@
+import { useReducer } from 'react'
 import '../styles/login.css'
 import {Link} from 'react-router-dom'
+import { useError } from '../customHooks/error'
 
 export function Register(){
-
-  
+  const [error,setError] = useError()
   return (
     <>
      <div className='main-container'>
@@ -15,16 +16,16 @@ export function Register(){
 
         <form>
           <label htmlFor="name">Name</label>
-          <input type="text" required placeholder='Enter name' id='name' />
-          <div className='error'></div>
+          <input onChange={e=>setError({type:"name",value : e.target.value})} type="text" required placeholder='Enter name' id='name' />
+          <div className='error'>{error.name}</div>
 
           <label htmlFor="email">Email</label>
-          <input type="email" required placeholder='Enter email' id='email'/>
-          <div className='error'></div>
+          <input onChange={e=>setError({type:"email",value : e.target.value})} type="email" required placeholder='Enter email' id='email'/>
+          <div className='error'>{error.email}</div>
 
           <label htmlFor="password">Password</label>
-          <input type="password" required placeholder='Enter password' id='password'/>
-          <div className='error'></div>
+          <input onChange={e => setError({type:"password",value : e.target.value})} type="password" required placeholder='Enter password' id='password'/>
+          <div className='error'>{error.password}</div>
           
           <input className='submit-btn' type="submit" value='Sign up now' />
           <div className='sign-up'> Already have an account?<Link to="/">Login </Link></div>
@@ -36,6 +37,7 @@ export function Register(){
 }
 
 export default function Login (){
+  const [error,setError] = useError()
   return (
     <>
     <div className='main-container'>
@@ -47,12 +49,12 @@ export default function Login (){
 
         <form>
           <label htmlFor='email'>Email</label>
-          <input autoComplete='true' type='text' placeholder='Enter your email' id='email' required />
-          <div className='error'></div>
+          <input onChange={e => setError({type:"email",value : e.target.value})} autoComplete='true' type='text' placeholder='Enter your email' id='email' required />
+          <div className='error'>{error.email}</div>
 
           <label htmlFor='password'>Password</label>
-          <input autoComplete='true' type='password' placeholder='Enter password' id='password' required />
-          <div className='error'></div>
+          <input onChange={e => setError({type:"password",value : e.target.value})} autoComplete='true' type='password' placeholder='Enter password' id='password' required />
+          <div className='error'>{error.password}</div>
 
           <div className='forgot-div'>Forgot password?</div>
 
---
+++ wed-house/src/customHooks/error.jsx
@@ -0,0 +1,42 @@
+import { useReducer } from "react";
+
+export function  useError (){
+  const [error,setError] = useReducer(updateError,{})
+
+  function updateError(error,action){
+    const errorObj = {...error}
+    switch(action.type){
+      case "name":
+        const value = action.value.trim();
+        if(!value){
+          errorObj.name = "Name is important!"
+        }else{
+          errorObj.name = ""
+        }
+        return errorObj;
+
+      case "password":
+        const length = action.value.length;
+        if(length < 6){
+          errorObj.password = "Password should be 6 charecter!"
+        }else{
+          errorObj.password = ""
+        }
+        return errorObj;
+
+      case "email" :
+        const email = action.value;
+        if(!email.includes('@')){
+          errorObj.email = "Enter a valid email!"
+        }else{
+          errorObj.email = ""
+        }
+        return errorObj ;
+
+      default :
+        return errorObj
+    }
+  }
+
+  return [error,setError]
+}
